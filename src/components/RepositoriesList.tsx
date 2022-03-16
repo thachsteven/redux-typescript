a3aa6b035b69024f4e5f370b0be087ec8f6e4ff2
Nguyen Ngoc Thach <thachsteven.marketing@gmail.com>
2022-03-16 14:56:52 +0700

    Call api with redux, action creators

--- src/components/RepositoriesList.tsx
+++ src/components/RepositoriesList.tsx
@@ -1,8 +1,20 @@
+import { useState } from "react";
+import { useActions } from "../hooks/userActions";
+
 const RepositoriesList: React.FC = () => {
+  const [term, setTerm] = useState("");
+  const { searchRepositories } = useActions();
+
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    searchRepositories(term);
+  };
+
   return (
     <div>
-      <form>
-        <input />
+      <form onSubmit={onSubmit}>
+        <input value={term} onChange={(e) => setTerm(e.target.value)} />
         <button>Search</button>
       </form>
     </div>
